49cff3a3d43c6feb45e377611786ed623c6a2842
Sergio Reis <sergioc_reis@hotmail.com>
2021-04-21 09:20:40 -0300

    refactor code to put in MVP Standard

--- src/controllers/SettingsControllers.ts
+++ src/controllers/SettingsControllers.ts
@@ -1,28 +1,33 @@
 import { Request, Response } from "express"
-import { getCustomRepository } from "typeorm"
-import { SettingsRepository } from "../repositories/SettingsRepository"
+import { SettingsService } from "../services/SettingsServices"
+
 
 
 class SettingsControllers {
     async create(req: Request, res: Response) {
         // Desestrutura o body
         const { chat, username } = req.body
-        const settingsRepository = getCustomRepository(SettingsRepository)
 
-        // Criar objeto referenciando a estrutura da tabela
-        const settings = settingsRepository.create({
-            chat,
-            username
-        })
+        const settingsService = new SettingsService()
 
-        await settingsRepository.save(settings)
-        /**
-         * Retorna JSON com Resultado do Insert, para funcionar é necessario ir no aquivo:
-         * server.ts e incluir a linh a abaixo senão o postman ou outro ferramenta não conseue gerar o retorno via JSON
-         * 
-         * app.use(express.json())
-         */
-        return res.json(settings)
+        // Dentro do Try colocamos o caminho de sucesso
+        try {
+            const settings = await settingsService.create({ chat, username })
+
+            /**
+             * Retorna JSON com Resultado do Insert, para funcionar é necessario ir no aquivo:
+             * server.ts e incluir a linh a abaixo senão o postman ou outro ferramenta não conseue gerar o retorno via JSON
+             * 
+             * app.use(express.json())
+             */
+            return res.json(settings)
+
+            // Dentro  catch colocamos o que fazer se der erro
+        } catch (err) {
+            return res.status(400).json({
+                message: err.message,
+            })
+        }
 
     }
 
---
+++ src/services/SettingsServices.ts
@@ -0,0 +1,37 @@
+import { throws } from "node:assert"
+import { getCustomRepository } from "typeorm"
+import { SettingsRepository } from "../repositories/SettingsRepository"
+
+
+interface ISettingsCreate {
+    chat: boolean
+    username: string
+}
+
+class SettingsService {
+    async create({ chat, username }: ISettingsCreate) {
+        const settingsRepository = getCustomRepository(SettingsRepository)
+
+        //  similar a SELECT * FROM SETTINGS WHERE USERNAME ="USERNAME" LIMIT 1
+        const userAlreadyExists = await settingsRepository.findOne({
+            username
+        })
+
+        if (userAlreadyExists){
+            throw new Error("User Already Exists in the Data Base")
+
+        }
+
+        // Criar objeto referenciando a estrutura da tabela
+        const settings = settingsRepository.create({
+            chat,
+            username
+        })
+
+        await settingsRepository.save(settings)
+
+        return settings
+    }
+}
+
+export { SettingsService }
